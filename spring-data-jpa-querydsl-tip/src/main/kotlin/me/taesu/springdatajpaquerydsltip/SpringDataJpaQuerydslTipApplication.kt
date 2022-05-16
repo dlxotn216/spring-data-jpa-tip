2829dd00b21b7aff9300bbbbbee07de276002d2b
Lee Tae Su <taesu@crscube.co.kr>
2022-05-16 10:47:36 +0900

    Initial Commit

---
+++ spring-data-jpa-querydsl-tip/src/main/kotlin/me/taesu/springdatajpaquerydsltip/SpringDataJpaQuerydslTipApplication.kt
@@ -0,0 +1,11 @@
+package me.taesu.springdatajpaquerydsltip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringDataJpaQuerydslTipApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringDataJpaQuerydslTipApplication>(*args)
+}
---
+++ spring-data-jpa-querydsl-tip/src/test/kotlin/me/taesu/springdatajpaquerydsltip/SpringDataJpaQuerydslTipApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.taesu.springdatajpaquerydsltip
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDataJpaQuerydslTipApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
